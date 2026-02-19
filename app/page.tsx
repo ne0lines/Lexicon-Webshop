80be082444f7615ac81c40495036af1df4427868
ne0lines <hermansson.david@gmail.com>
2026-02-19 15:00:00 +0100

    Refactor Home component layout and add product management features

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import type { ProductsResponse } from "./types";
+import { Plus, Barcode, ChartLine, ShoppingCart, Users, Settings, Package2, CircleCheck, TriangleAlert, CircleX } from 'lucide-react';
 
 const API_URL = "http://localhost:4000";
 const defaultLimit = "6";
@@ -16,9 +17,85 @@ export default async function Home() {
 console.log(products);
 
   return (
-    <main>
-      <h1>Products</h1>
-      <div>{products.map((product) => <h2 key={product.id}>{product.title} - {product.category?.name}</h2>)}</div>
-    </main>
+    <>
+      <aside className="h-full fixed p-6">
+        <div className="flex flex-col">
+          <span className="font-bold text-xl">Webbutiken</span>
+          <span className="font-normal text-xs clear-left">Admin panel</span>
+        </div>
+        <nav className="p-6">
+          <ul className="flex flex-col">
+            <li className="flex h-8 gap-4"><Barcode/>Products</li>
+            <li className="flex h-8 gap-4"><ChartLine/>Analytics</li>
+            <li className="flex h-8 gap-4"><ShoppingCart/>Orders</li>
+            <li className="flex h-8 gap-4"><Users/>Customers</li>
+            <li className="flex h-8 gap-4"><Settings/>Settings</li>
+          </ul>
+        </nav>
+        <section id="user-info">
+          <span>avatar</span>
+          <strong>username</strong>
+          <span>user e-mail</span>
+        </section>
+      </aside>
+      <header className="w-full fixed pl-70">
+        <h1>Product management</h1>
+        <span>Manage your store inventory</span>
+        <button><Plus/> Add product</button>
+        <div>
+          <span>Total products</span><Package2 />
+          <span>248</span>
+          </div> 
+        <div>
+          <span>In stock</span><CircleCheck />
+          <span>189</span>
+          </div>
+        <div>
+          <span>Low stock</span><TriangleAlert />
+          <span>34</span>
+          </div>
+        <div>
+          <span>Out of stock</span><CircleX />
+          <span>25</span>
+          </div>
+      </header>
+      <main className="w-full pl-70 pt-80">
+        <section>
+          <label htmlFor="search-input">Sök bland produkter</label>
+          <input name="search-input" id="search-input" />
+          <label htmlFor="category" >Välj kategori</label>
+          <select id="category">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="2">3</option>
+          </select>
+          <label htmlFor="status" >Välj lagerstatus</label>
+          <select id="category">
+            <option value="1">High</option>
+            <option value="2">Medium</option>
+            <option value="2">Low</option>
+          </select>
+          <label htmlFor="filter">Filtrera produkter</label>
+          <button type="button" id="filter">Filter</button>
+        </section>
+        
+        <h1>Products</h1>
+        <div>{products.map((product) => <h2 key={product.id}>{product.title} - {product.category?.name}</h2>)}</div>
+        <div class="flex">
+          <div >Showing X to Y of Z products</div>
+          <div>
+            <nav>
+              <ul>
+                <li>Previous</li>
+                <li class="active">1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>Next</li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
