b3093db312e1a5e6d5cec6a77a7c816c81e95d3f
henrietta jeansson <henriettajeansson@gmail.com>
2026-02-23 14:09:12 +0100

    added new component to recieve statistic props in header

---
+++ app/components/HeaderStatCard.tsx
@@ -0,0 +1,72 @@
+import { CircleCheck, CircleX, Package2, Plus, TriangleAlert } from "lucide-react";
+
+export interface StatCardProps {
+    total: number;
+    lowStock: number;
+    outOfStock: number;
+
+}
+// Each card will recieve a differenet prop
+export default function StatCard() {
+
+    // Get total numbers from props in statistic bar
+
+    // Total products
+
+    // How many in stock
+
+    // How many have "low stock"
+
+    // How many products are out of stock
+    
+
+    return (
+        <>
+            <section className="px-8 py-4 bg-white flex items-center justify-between">
+                <div >
+                    <h1 className="text-xl font-bold">Product management</h1>
+                    <span className="text-gray-500 text-sm">Manage your store inventory</span>
+                </div>
+                <button className="bg-purple-600 text-white px-4 py-2 rounded-lg flex items-center gap-2 font-medium hover:bg-purple-700"><Plus /> Add product</button>
+            </section>
+            <section className="grid grid-cols-4 gap-6 mb-8 mt-24 px-8 py-4 bg-gray-50">
+                <div className="bg-white rounded shadow p-4 flex  items-center justify-between">
+                    <div className="flex flex-col items-start">
+                        <span className="text-xs text-gray-500 mb-1">Total products</span>
+                        <span className="block text-2xl font-bold">248</span>
+                    </div>
+                    <div className="bg-purple-100 rounded-md p-2 flex items-center justify-center text-purple-500">
+                        <Package2 />
+                    </div>
+                </div>
+                <div className="bg-white rounded shadow p-4 flex  items-center justify-between">
+                    <div className="flex flex-col items-start">
+                        <span className="text-xs text-gray-500 mb-1">In stock</span>
+                        <span className="block text-2xl font-bold">189</span>
+                    </div>
+                    <div className="bg-green-100 rounded-md p-2 flex items-center justify-center text-green-500">
+                        <CircleCheck />
+                    </div>
+                </div>
+                <div className="bg-white rounded shadow p-4 flex  items-center justify-between">
+                    <div className="flex flex-col items-start">
+                        <span className="text-xs text-gray-500 mb-1">Low stock</span>
+                        <span className="block text-2xl font-bold">34</span>
+                    </div>
+                    <div className="bg-yellow-100 rounded-md p-2 flex items-center justify-center text-yellow-500">
+                        <TriangleAlert />
+                    </div>
+                </div>
+                <div className="bg-white rounded shadow p-4 flex  items-center justify-between">
+                    <div className="flex flex-col items-start">
+                        <span className="text-xs text-gray-500 mb-1">Out of stock</span>
+                        <span className="block text-2xl font-bold">25</span>
+                    </div>
+                    <div className="bg-pink-100 rounded-md p-2 flex items-center justify-center text-pink-500">
+                        <CircleX />
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
