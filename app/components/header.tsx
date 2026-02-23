a85afb4da2519fdbc60ce83081f42424fce6b4bf
henrietta jeansson <henriettajeansson@gmail.com>
2026-02-23 14:32:06 +0100

    moved header jsx out of main page and into header component. Need to add mathematics to add to props for dynamic showing of statistics

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,3 +1,4 @@
+import { Funnel, Search } from "lucide-react";
 import StatCard from "./HeaderStatCard";
 
 interface Header {
@@ -11,7 +12,48 @@ export default function Header({ total, inStock, lowStock, outOfStock }: Header)
 
     return (
         <>
-            <StatCard total={total} inStock={inStock} lowStock={lowStock} outOfStock={outOfStock} />
+            <header className="fixed left-64 top-0 right-0 z-30">
+
+                <section className="px-8 py-4 bg-white flex items-center justify-between">
+                    <div >
+                        <h1 className="text-xl font-bold">Product management</h1>
+                        <span className="text-gray-500 text-sm">Manage your store inventory</span>
+                    </div>
+                    <button className="bg-purple-600 text-white px-4 py-2 rounded-lg flex items-center gap-2 font-medium hover:bg-purple-700"><Plus /> Add product</button>
+                </section>
+
+                <StatCard total={total} inStock={inStock} lowStock={lowStock} outOfStock={outOfStock} />
+
+
+                <form className="flex items-center text-xs gap-4 mb-6 border-gray-300 px-8 py-4 bg-white">
+                    <div className="border-gray-300 border-2 rounded-lg px-4 py-2 flex items-center flex-1">
+                        <Search className="w-4 h-4 pink-500" />
+                        <label htmlFor="search-input" className="hidden" >Sök bland produkter</label>
+                        <input name="search-input" placeholder="Search products..." id="search-input" />
+                    </div>
+                    <div className="border-gray-300 border-2 rounded-lg px-4 py-2 flex items-center">
+                        <label htmlFor="category" className="hidden">Välj kategori</label>
+                        <select id="category">
+                            <option value="1">Category 1</option>
+                            <option value="2">Category 2</option>
+                            <option value="2">Category 3</option>
+                        </select>
+                    </div>
+                    <div className="border-gray-300 border-2 rounded-lg px-4 py-2 flex items-center">
+                        <label htmlFor="status" className="hidden">Välj lagerstatus</label>
+                        <select id="category">
+                            <option value="1">High</option>
+                            <option value="2">Medium</option>
+                            <option value="3">Low</option>
+                        </select>
+                    </div>
+                    <div className="border-gray-300 border-2 rounded-lg px-4 py-2 flex items-center">
+                        <Funnel className="w-4 h-4" />
+                        <label htmlFor="filter" className="hidden">Filtrera produkter</label>
+                        <button type="button" id="filter">Filter</button>
+                    </div>
+                </form>
+            </header>
         </>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Header from "./components/header";
 import type { ProductsResponse } from "./types";
 import { Plus, Package2, CircleCheck, TriangleAlert, CircleX, Funnel, SquarePen, Trash2, Search } from 'lucide-react';
 
@@ -21,47 +22,7 @@ export default async function Home() {
     <>
 
       {/* Replace with header component */}
-      <header className="fixed left-64 top-0 right-0 z-30">
-
-        <section className="px-8 py-4 bg-white flex items-center justify-between">
-          <div >
-            <h1 className="text-xl font-bold">Product management</h1>
-            <span className="text-gray-500 text-sm">Manage your store inventory</span>
-          </div>
-          <button className="bg-purple-600 text-white px-4 py-2 rounded-lg flex items-center gap-2 font-medium hover:bg-purple-700"><Plus /> Add product</button>
-        </section>
-
-
-
-        <form className="flex items-center text-xs gap-4 mb-6 border-gray-300 px-8 py-4 bg-white">
-          <div className="border-gray-300 border-2 rounded-lg px-4 py-2 flex items-center flex-1">
-            <Search className="w-4 h-4 pink-500" />
-            <label htmlFor="search-input" className="hidden" >Sök bland produkter</label>
-            <input name="search-input" placeholder="Search products..." id="search-input" />
-          </div>
-          <div className="border-gray-300 border-2 rounded-lg px-4 py-2 flex items-center">
-            <label htmlFor="category" className="hidden">Välj kategori</label>
-            <select id="category">
-              <option value="1">Category 1</option>
-              <option value="2">Category 2</option>
-              <option value="2">Category 3</option>
-            </select>
-          </div>
-          <div className="border-gray-300 border-2 rounded-lg px-4 py-2 flex items-center">
-            <label htmlFor="status" className="hidden">Välj lagerstatus</label>
-            <select id="category">
-              <option value="1">High</option>
-              <option value="2">Medium</option>
-              <option value="3">Low</option>
-            </select>
-          </div>
-          <div className="border-gray-300 border-2 rounded-lg px-4 py-2 flex items-center">
-            <Funnel className="w-4 h-4" />
-            <label htmlFor="filter" className="hidden">Filtrera produkter</label>
-            <button type="button" id="filter">Filter</button>
-          </div>
-        </form>
-      </header>
+      <Header total={total} inStock={inStock} lowStock={lowStock} outOfStock={outOfStock} />
       <main className="w-full pl-70 pt-70 pb-15 bg-gray-50">
         <div className="bg-pink-100 rounded-md p-2 flex items-center justify-center text-pink-500">
           <CircleX />
