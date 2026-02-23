aa9975d76febe066cf7a6501ed388d92770a644f
henrietta jeansson <henriettajeansson@gmail.com>
2026-02-23 14:20:29 +0100

    update header and statCard components with props

--- app/components/HeaderStatCard.tsx
+++ app/components/HeaderStatCard.tsx
@@ -1,13 +1,14 @@
 import { CircleCheck, CircleX, Package2, Plus, TriangleAlert } from "lucide-react";
 
-export interface StatCardProps {
+interface StatCardProps {
     total: number;
+    inStock: number;
     lowStock: number;
     outOfStock: number;
 
 }
 // Each card will recieve a differenet prop
-export default function StatCard() {
+export default function StatCard({ total, inStock, lowStock, outOfStock }: StatCardProps) {
 
     // Get total numbers from props in statistic bar
 
@@ -18,22 +19,16 @@ export default function StatCard() {
     // How many have "low stock"
 
     // How many products are out of stock
-    
+
 
     return (
         <>
-            <section className="px-8 py-4 bg-white flex items-center justify-between">
-                <div >
-                    <h1 className="text-xl font-bold">Product management</h1>
-                    <span className="text-gray-500 text-sm">Manage your store inventory</span>
-                </div>
-                <button className="bg-purple-600 text-white px-4 py-2 rounded-lg flex items-center gap-2 font-medium hover:bg-purple-700"><Plus /> Add product</button>
-            </section>
+
             <section className="grid grid-cols-4 gap-6 mb-8 mt-24 px-8 py-4 bg-gray-50">
                 <div className="bg-white rounded shadow p-4 flex  items-center justify-between">
                     <div className="flex flex-col items-start">
                         <span className="text-xs text-gray-500 mb-1">Total products</span>
-                        <span className="block text-2xl font-bold">248</span>
+                        <span className="block text-2xl font-bold">{total}</span>
                     </div>
                     <div className="bg-purple-100 rounded-md p-2 flex items-center justify-center text-purple-500">
                         <Package2 />
@@ -42,7 +37,7 @@ export default function StatCard() {
                 <div className="bg-white rounded shadow p-4 flex  items-center justify-between">
                     <div className="flex flex-col items-start">
                         <span className="text-xs text-gray-500 mb-1">In stock</span>
-                        <span className="block text-2xl font-bold">189</span>
+                        <span className="block text-2xl font-bold">{inStock}</span>
                     </div>
                     <div className="bg-green-100 rounded-md p-2 flex items-center justify-center text-green-500">
                         <CircleCheck />
@@ -51,7 +46,7 @@ export default function StatCard() {
                 <div className="bg-white rounded shadow p-4 flex  items-center justify-between">
                     <div className="flex flex-col items-start">
                         <span className="text-xs text-gray-500 mb-1">Low stock</span>
-                        <span className="block text-2xl font-bold">34</span>
+                        <span className="block text-2xl font-bold">{lowStock}</span>
                     </div>
                     <div className="bg-yellow-100 rounded-md p-2 flex items-center justify-center text-yellow-500">
                         <TriangleAlert />
@@ -60,7 +55,7 @@ export default function StatCard() {
                 <div className="bg-white rounded shadow p-4 flex  items-center justify-between">
                     <div className="flex flex-col items-start">
                         <span className="text-xs text-gray-500 mb-1">Out of stock</span>
-                        <span className="block text-2xl font-bold">25</span>
+                        <span className="block text-2xl font-bold">{outOfStock}</span>
                     </div>
                     <div className="bg-pink-100 rounded-md p-2 flex items-center justify-center text-pink-500">
                         <CircleX />
